6a3e7bd9ed183917034e269fa99ce4e747051371
Rodrigo Costa <rodrigocosta.rsc2878@gmail.com>
2026-06-07 15:04:44 +0100

    added ldir
    listing directories

--- src/Shell.kt
+++ src/Shell.kt
@@ -8,20 +8,23 @@ class Shell {
     fun prepare(cmd: Command):ShellCommand = when (cmd.name) {
         "quit","exit" -> ShellCommand.ShellExit
         "history"->ShellCommand.ShellHistory
-        "ldir" ->ShellCommand.ShellListDir(currentDir)
+        "ldir" ->ShellCommand.ShellListDir(if (cmd.args.isEmpty()) currentDir else cmd.args[0])
         "cdir" ->ShellCommand.ShellChangeDir(cmd.args[0])
         else -> ShellCommand.UnknownCommand(cmd.name)
     }
     fun execute(prepared:ShellCommand,cmd: Command ){
         when(prepared){
             is ShellCommand.ShellExit -> exitProcess(0)
-            is ShellCommand.ShellChangeDir -> currentDir=prepared.path
             is ShellCommand.ShellHistory -> {
                 for ((key,value) in history) {
                     println("$key --> $value")
                 }
             }
-            is ShellCommand.ShellListDir -> {}
+            is ShellCommand.ShellChangeDir -> currentDir=prepared.path
+            is ShellCommand.ShellListDir -> {
+                val dir=java.io.File(prepared.path)
+                dir.listFiles()?.forEach { println(it.name) } ?: println("No such directory/Directory is empty")
+            }
             is ShellCommand.UnknownCommand -> println("Unknown command: ${prepared.cmd}")
         }
         history[(LocalDateTime.now()).toString()] = cmd.raw
