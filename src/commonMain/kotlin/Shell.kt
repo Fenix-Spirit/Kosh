ed9568a759274a87887502e17974ed877b291fa2
Rodrigo Costa <rodrigocosta.rsc2878@gmail.com>
2026-06-12 01:30:54 +0100

    persistent history

--- src/commonMain/kotlin/Shell.kt
+++ src/commonMain/kotlin/Shell.kt
@@ -1,13 +1,53 @@
 import commands.Command
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlin.experimental.ExperimentalNativeApi
+import kotlinx.cinterop.toKString
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlinx.io.buffered
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
+import kotlinx.io.readLine
+import kotlinx.io.writeString
+import platform.posix.getenv
 import kotlin.system.exitProcess
+@OptIn( ExperimentalForeignApi::class,ExperimentalNativeApi::class)
 class Shell {
+    private val root: Path? = when (Platform.osFamily) {
+        OsFamily.LINUX, OsFamily.MACOSX -> getenv("HOME")?.toKString()?.let { Path("$it/.kosh") }
+        OsFamily.WINDOWS -> getenv("APPDATA")?.toKString()?.let { Path("$it/.kosh") }
+        else -> null
+    }
+    private val histPath=Path(root?: Path("./"),".kosh-history")
     private val history: MutableMap<String, String> = mutableMapOf()
     private var currentDir: String = "/"
+    init {
+        if (SystemFileSystem.exists(root?: Path("./"))) {
+            if(SystemFileSystem.exists(histPath)){
+                try {
+                    SystemFileSystem.source(histPath).buffered().use {source ->
+                        while (!source.exhausted()){
+                            val line=source.readLine()
+                            if (line!=null) {
+                                history[line.split("-->")[0]] = line.split("-->").drop(1).joinToString(" ")
+                            }
+                        }
+                        source.close()
+                    }
+                }
+                catch (e:Exception){
+                    println("Error reading history file")
+                }
+            }
+            else{
+                SystemFileSystem.createDirectories(histPath)
+            }
+        }
+        else {
+            SystemFileSystem.createDirectories(root?: Path("./.kosh"))
+        }
+    }
     fun prepare(cmd: Command):ShellCommand = when (cmd.name) {
         "quit","exit" -> ShellCommand.ShellExit
         "history"->ShellCommand.ShellHistory
@@ -28,11 +68,16 @@ class Shell {
             is ShellCommand.ShellChangeDir -> currentDir=prepared.path
             is ShellCommand.ShellListDir -> {
                 val path=Path(prepared.path)
-                SystemFileSystem.list(path).forEach { println(it.name) }            }
+                SystemFileSystem.list(path).forEach { println(it.name) }
+            }
             is ShellCommand.ShellDir -> println(currentDir)
             is ShellCommand.ShellPrint -> println(prepared.msg)
             is ShellCommand.UnknownCommand -> println("Unknown command: ${prepared.cmd}")
         }
-        history[(Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())).toString()] = cmd.raw
+        val currTime=Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+        history[currTime.toString()] = cmd.raw
+        SystemFileSystem.sink(histPath,true).buffered().use {sink ->
+            sink.writeString(currTime.toString()+" --> ${cmd.raw}\n")
+        }
     }
 }
